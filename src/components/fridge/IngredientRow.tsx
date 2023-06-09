518c5eb9e28da92f9b76f2088929a03ebc86882b
Mateusz Lichota <mateusz@lichota.net>
2023-06-09 03:12:00 +0200

    Make fridge tab styles a bit better

--- src/components/fridge/IngredientRow.tsx
+++ src/components/fridge/IngredientRow.tsx
@@ -38,18 +38,18 @@ function IngredientRow({ ingredient, refresh }: Props) {
             <td>{ingredient.name}</td>
             <td>{ingredient.expiry} days</td>
             <td>
-                <div className={getStyle(styles, "smallCircle")}>
+                <button className={getStyle(styles, "smallCircle")}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" className={getStyle(styles, "icon")}>
                         <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
                     </svg>
-                </div>
+                </button>
             </td>
             <td className={getStyle(styles, "rightEdge")}>
-                <div className={getStyle(styles, "smallCircle")} onClick={() => deleteIngredient(ingredient.id)}>
+                <button className={getStyle(styles, "smallCircle")} onClick={() => deleteIngredient(ingredient.id)}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" className={getStyle(styles, "icon")}>
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                     </svg>
-                </div>
+                </button>
             </td>
         </tr>
     )
