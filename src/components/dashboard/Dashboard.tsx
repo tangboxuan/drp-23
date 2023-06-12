3d47e39d4e99e8000c2d0041ea106c9feef278f9
Jakub Michalski <j-michalski@outlook.com>
2023-06-12 22:15:21 +0100

    Introduce persona to app

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -10,7 +10,7 @@ function Dashboard() {
   return (
     <>
       <div className={getStyle(styles, "container")}>
-        <h1 className={getStyle(styles, "title")}>Hi, Ryan</h1>
+        <h1 className={getStyle(styles, "title")}>Hi, Emma</h1>
 
         <h1 className={getStyle(styles, "subtitle")}>My fridge</h1>
 
